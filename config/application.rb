466ce3ac1371dfb91b9b452e633ad52677d81299
Rod <rcardenas@utexas.edu>
2016-08-23 19:47:48 -0700

    Enable public API.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Flow
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => "*",
+      'Access-Control-Request-Method' => "GET"
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
